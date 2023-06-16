10d85e15c15280b2c2f93ca256e2f503ec736738
OM <omsharma9392@gmail.com>
2023-06-16 22:47:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/familysefty/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.familysefty
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.familysefty.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.nav_home -> callFragment(HomeFragment.newInstance())
+                R.id.nav_dashboard -> callFragment(GuardFragment.newInstance())
+                R.id.nav_guard -> callFragment(GuardFragment.newInstance())
+                R.id.nav_user -> callFragment(GuardFragment.newInstance())
+            }
+            true
+        }
+
+    }
+
+    fun callFragment(fragment: Fragment)
+    {
+        var transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameContainer, fragment)
+        transaction.commit()
+    }
+}
